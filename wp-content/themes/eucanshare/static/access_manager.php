82572e19e6df1183b8bd2d31d6c0ae2cdc8cc98f
Josep Ll. Gelpi <gelpi@ub.edu>
2019-10-08 15:28:09 +0000

    progres home

--- wp-content/themes/eucanshare/static/access_manager.php
+++ wp-content/themes/eucanshare/static/access_manager.php
@@ -9,7 +9,7 @@
 <div class="item cf">
 	<div class="wrapper cf">
 		<div class="cf">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+The Access Manager is a user interface for researchers to request access to cohort data and acquire user credentials, as well as for cohort owners to grant and manage access, based on a blockchain consent model.
 		</div>
 	</div>
 </div>
